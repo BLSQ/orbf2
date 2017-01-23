f72dd91109f97b970b155d1356a5436cab4f5212
Stéphan Mestach <smestach@bluesquare.org>
2017-01-23 07:40:34 +0100

    Secure admin, remove sidekiq reference.

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,5 @@
 
 Rails.application.routes.draw do
-  if ENV["ADMIN_PASSWORD"]
-    Sidekiq::Web.use Rack::Auth::Basic do |username, password|
-      username == "admin" && password == ENV["ADMIN_PASSWORD"]
-    end
-  end
-
   mount RailsAdmin::Engine => "/admin", as: "rails_admin"
 
   devise_for :users
