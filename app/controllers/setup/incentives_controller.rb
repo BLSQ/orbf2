fc3f8f6a0d478ca1b5af92ab39b3d7a2e11addb1
Stéphan Mestach <smestach@bluesquare.org>
2017-01-23 09:20:43 +0100

    Fix incentive display

--- app/controllers/setup/incentives_controller.rb
+++ app/controllers/setup/incentives_controller.rb
@@ -15,28 +15,28 @@ class Setup::IncentivesController < PrivateController
   def create
     @incentive = IncentiveConfig.new(incentive_params)
     @project = current_project
-    @incentive.package = @project.packages.find(@incentive.package_id) if @incentive.package_id
-    @incentive.state = State.find(@incentive.state_id) if @incentive.state_id
-    @incentive.project = current_project
+    incentive.package = @project.packages.find(incentive.package_id) if incentive.package_id
+    incentive.state = State.find(incentive.state_id) if incentive.state_id
+    incentive.project = current_project
 
-    if @incentive.valid?
+    if incentive.valid?
       dhis2 = incentive.project.dhis2_connection
 
-      if @incentive.entity_groups
+      if incentive.entity_groups
 
-        org_units = @incentive.project.dhis2_connection.organisation_units.list(filter: "organisationUnitGroups.id:in:[#{@incentive.entity_groups.join(',')}]", page_size: 15)
-        @incentive.entities = org_units
+        org_units = incentive.project.dhis2_connection.organisation_units.list(filter: "organisationUnitGroups.id:in:[#{@incentive.entity_groups.join(',')}]", page_size: 15)
+        incentive.entities = org_units
       end
 
-      if @incentive.activity_incentives && params[:set_values]
+      if incentive.activity_incentives && params[:set_values]
 
-        org_units = @incentive.project.dhis2_connection.organisation_units.list(filter: "organisationUnitGroups.id:in:[#{@incentive.entity_groups.join(',')}]", page_size: 50_000)
+        org_units = incentive.project.dhis2_connection.organisation_units.list(filter: "organisationUnitGroups.id:in:[#{incentive.entity_groups.join(',')}]", page_size: 50_000)
 
-        status = @incentive.set_data_elemets_values
+        status = incentive.set_data_elemets_values
 
         if status.success?
           ttl_affected = status.raw_status["import_count"]["imported"] + status.raw_status["import_count"]["updated"]
-          flash[:success] = "#{@incentive.state.name}-#{@incentive.package.name} created successfuly for #{ttl_affected} Org. Units"
+          flash[:success] = "#{incentive.state.name}-#{incentive.package.name} created successfuly for #{ttl_affected} Org. Units"
           redirect_to(root_path)
         end
 
