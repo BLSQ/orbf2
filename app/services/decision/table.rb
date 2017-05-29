100e362a9a81ed0afe96dbc94fd5d6698e3a6fbe
Stéphan Mestach <smestach@bluesquare.org>
2017-05-29 12:06:04 +0200

    minimal table version

---
+++ app/services/decision/table.rb
@@ -0,0 +1,48 @@
+require "csv"
+module Decision
+  class Rule
+    def initialize(headers, row)
+      @headers = headers
+      @row = headers.map do |header|
+        [header, row[header] ? row[header].strip : nil]
+      end.to_h
+    end
+
+    def matches?(hash)
+      headers("in:").all? { |header| hash[header] == @row[header] || @row[header].nil?}
+    end
+
+    def headers(type)
+      @headers.select {|header| header.start_with?(type)}
+    end
+
+    def apply(hash)
+      headers("out:").each do |header|
+        hash[header.slice(4..-1)] = @row[header]
+      end
+      hash
+    end
+  end
+
+  class Table
+    def initialize(csv_string)
+      csv = CSV.parse(csv_string, headers: true)
+      @headers = csv.headers
+
+      @rules = csv.map do |row|
+        Rule.new(@headers, row)
+      end
+    end
+
+    def find(raw_hash)
+      hash = raw_hash.map do |k, v|
+        ["in:#{k}", v]
+      end.to_h
+      @rules.each do |rule|
+        next unless rule.matches?(hash)
+        return rule.apply(hash)
+      end
+      byebug
+    end
+  end
+end
