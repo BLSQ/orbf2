ffe64742cd5037c86ee9b0701d59c7b02338748a
butofleury <fleurybu@gmail.com>
2017-06-14 16:40:22 +0200

    Add new migration for version

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170606060925) do
+ActiveRecord::Schema.define(version: 20170613165843) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -106,6 +106,14 @@ ActiveRecord::Schema.define(version: 20170606060925) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.index ["rule_id"], name: "index_formulas_on_rule_id", using: :btree
+  end
+
+  create_table "indicators", force: :cascade do |t|
+    t.string   "name"
+    t.float    "price"
+    t.string   "dhis2_verified_id"
+    t.datetime "created_at",        null: false
+    t.datetime "updated_at",        null: false
   end
 
   create_table "package_entity_groups", force: :cascade do |t|
@@ -228,6 +236,26 @@ ActiveRecord::Schema.define(version: 20170606060925) do
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
     t.index ["program_id"], name: "index_users_on_program_id", using: :btree
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  end
+
+  create_table "version_associations", force: :cascade do |t|
+    t.integer "version_id"
+    t.string  "foreign_key_name", null: false
+    t.integer "foreign_key_id"
+    t.index ["foreign_key_name", "foreign_key_id"], name: "index_version_associations_on_foreign_key", using: :btree
+    t.index ["version_id"], name: "index_version_associations_on_version_id", using: :btree
+  end
+
+  create_table "versions", force: :cascade do |t|
+    t.string   "item_type",      null: false
+    t.integer  "item_id",        null: false
+    t.string   "event",          null: false
+    t.string   "whodunnit"
+    t.text     "object"
+    t.datetime "created_at"
+    t.integer  "transaction_id"
+    t.index ["item_type", "item_id"], name: "index_versions_on_item_type_and_item_id", using: :btree
+    t.index ["transaction_id"], name: "index_versions_on_transaction_id", using: :btree
   end
 
   add_foreign_key "activities", "projects"
