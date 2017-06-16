8e07aa51747e809efdc55919fef11545fd4eed15
butofleury <fleurybu@gmail.com>
2017-06-16 16:55:54 +0200

    Add  user email in the info dislayed on the list of audit

--- app/controllers/setup/changes_controller.rb
+++ app/controllers/setup/changes_controller.rb
@@ -1,6 +1,12 @@
 class Setup::ChangesController < PrivateController
 
 	def index
-		@versions = Version.where(whodunnit: current_user)
+		@versions = Version.where(whodunnit: current_user).map do  |k| {
+				:item_type => k[:item_type],
+				:email => User.find_by_id(k[:whodunnit]).email,
+				:event => k[:event],
+				:object => k[:object]
+			}
+		end
 	end
 end
