0f9f6fd63467e54501b8cc0ee9f33be1c008098f
Nicolas Germeau <nicolas.germeau@gmail.com>
2017-07-24 16:58:59 +0200

    correcting invalid index while validating decision table

--- app/models/decision_table.rb
+++ app/models/decision_table.rb
@@ -29,7 +29,7 @@ class DecisionTable < ApplicationRecord
   def in_activity_code_exists
     return unless in_headers.include?("activity_code")
     available_codes = rule.package.activities.map(&:code).compact
-    invalid_rules = decision_table.rules.reject { |rule| available_codes.include?(rule["activity_code"]) }
+    invalid_rules = decision_table.rules.reject { |rule| available_codes.include?(rule["in:activity_code"]) }
     invalid_rules.each { |invalid_rule| errors[:content] << "#{invalid_rule} not in available package codes #{available_codes}!" }
   end
 
