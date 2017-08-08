26ada45fac429bda6640e2ab272eec822895b4c8
Stéphan Mestach <smestach@bluesquare.org>
2017-08-08 11:52:53 +0200

    Remove unused method and don't re-instantiate project_factory object

--- app/controllers/setup/seeds_controller.rb
+++ app/controllers/setup/seeds_controller.rb
@@ -4,7 +4,7 @@ class Setup::SeedsController < PrivateController
     project_anchor = current_user.program.project_anchor
     project_factory = ProjectFactory.new
     suffix = Time.now.to_s[0..15] + " - "
-    project = ProjectFactory.new.build(
+    project = project_factory.build(
       dhis2_url:      params[:local] ? "http://127.0.0.1:8085/" : "https://play.dhis2.org/demo",
       user:           "admin",
       password:       "district",
@@ -22,18 +22,5 @@ class Setup::SeedsController < PrivateController
     Dhis2SnapshotWorker.new.perform(project_anchor.id)
     flash[:notice] = " created package and rules for #{suffix} : #{project.packages.map(&:name).join(', ')}"
     redirect_to root_path
-  end
-
-  def update_package_with_dhis2(package, suffix, states, groups, acitivity_ids)
-    package.name += suffix
-    package.states = states
-    groups.each_with_index do |group, index|
-      peg = package.package_entity_groups[index]
-      group.each do |k, v|
-        peg[k] = v
-      end
-    end
-    created_ged = package.create_data_element_group(acitivity_ids)
-    package.data_element_group_ext_ref = created_ged.id
   end
 end
