66ce39aa260d18ff6db716f69168009e22f7c2c1
Stéphan Mestach <smestach@bluesquare.org>
2017-08-08 14:05:46 +0200

    Spec for entity_builder

--- app/services/invoicing/entity_builder.rb
+++ app/services/invoicing/entity_builder.rb
@@ -4,12 +4,16 @@ module Invoicing
       Analytics::Entity.new(
         org_unit.id,
         org_unit.name,
-        org_unit.organisation_unit_groups.map { |n| n["id"] },
+        to_group_ids(org_unit),
         to_facts(org_unit)
       )
     end
 
     private
+
+    def to_group_ids(org_unit)
+      org_unit.organisation_unit_groups.map { |n| n["id"] }
+    end
 
     def to_code(dhis2_ressource)
       code = dhis2_ressource.code || dhis2_ressource.short_name || dhis2_ressource.display_name
@@ -17,20 +21,24 @@ module Invoicing
     end
 
     def to_facts(org_unit)
+      parent_ids = org_unit.path.split("/").reject(&:empty?)
+      facts = parent_ids.each_with_index
+                        .map { |parent_id, index| ["level_#{index + 1}", parent_id] }
+                        .to_h
+      facts.merge(to_group_set_facts(org_unit))
+    end
+
+    def to_group_set_facts(org_unit)
       pyramid = org_unit.pyramid
-      group_set_facts = pyramid.org_unit_groups(org_unit.organisation_unit_groups.map { |n| n["id"] }).map do |group|
-        next if group.group_set_ids.empty?
+      group_set_facts = pyramid.org_unit_groups(to_group_ids(org_unit))
+                               .select { |group| group.group_set_ids.any? }
+                               .flat_map do |group|
         group.group_set_ids.map do |groupset_id|
           groupset = pyramid.org_unit_group_set(groupset_id)
           ["groupset_code_#{to_code(groupset)}", to_code(group)]
         end
       end
-
-      parent_ids = org_unit.path.split("/").reject(&:empty?)
-      facts = parent_ids.each_with_index
-                        .map { |parent_id, index| ["level_#{index + 1}", parent_id] }
-                        .to_h
-      facts.merge(group_set_facts.compact.flatten(1).to_h)
+      group_set_facts.to_h
     end
   end
 end
