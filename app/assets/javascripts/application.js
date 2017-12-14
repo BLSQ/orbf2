729ca0f702fa834246a0a65b08082551090e6483
Stéphan Mestach <smestach@bluesquare.org>
2017-12-14 09:30:17 +0100

    Fix layout and prevent multiple popover at the same time

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -30,4 +30,14 @@ $(document).ready(function() {
       return $(this).find(".popper-content").html();
     }
   });
+  // one popover at a time
+  $('body').on('click', function (e) {
+    $('[data-toggle="popover"]').each(function () {
+        //the 'is' for buttons that trigger popups
+        //the 'has' for icons within a button that triggers a popup
+        if (!$(this).is(e.target) && $(this).has(e.target).length === 0 && $('.popover').has(e.target).length === 0) {
+            $(this).popover('hide');
+        }
+    });
+});
 });
