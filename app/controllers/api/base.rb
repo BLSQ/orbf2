f238ae4fbb407d95f031c6eed537d0b9e65ad090
Stéphan Mestach <smestach@bluesquare.org>
2018-06-01 14:50:02 +0200

    Pronto fixes

--- app/controllers/api/base.rb
+++ app/controllers/api/base.rb
@@ -1,17 +1,21 @@
+module Api
+  class Base < ActionController::Base
+    before_action :set_permissive_cors_headers
 
-class Api::Base < ActionController::Base
-  before_action :set_permissive_cors_headers
+    private
 
-  private
+    def current_project_anchor
+      @current_project_anchor || ProjectAnchor.find_by!(token: params.fetch(:token))
+    end
 
-  def current_project_anchor
-    @current_project_anchor || ProjectAnchor.find_by!(token: params.fetch(:token))
-  end
+    ALL = "*".freeze
+    ALLOW_HEADERS = "Origin, X-Requested-With, Content-Type, Accept, Authorization".freeze
 
-  def set_permissive_cors_headers
-    headers["Access-Control-Allow-Origin"] = "*"
-    headers["Access-Control-Allow-Methods"] = "POST, PUT, DELETE, GET, OPTIONS"
-    headers["Access-Control-Request-Method"] = "*"
-    headers["Access-Control-Allow-Headers"] = "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+    def set_permissive_cors_headers
+      headers["Access-Control-Allow-Origin"] = ALL
+      headers["Access-Control-Allow-Methods"] = "POST, PUT, DELETE, GET, OPTIONS"
+      headers["Access-Control-Request-Method"] = ALL
+      headers["Access-Control-Allow-Headers"] = ALLOW_HEADERS
+    end
   end
 end
--- app/controllers/api/invoices_controller.rb
+++ app/controllers/api/invoices_controller.rb
@@ -1,16 +1,14 @@
 module Api
   class InvoicesController < Api::Base
     def create
-
       project_anchor = current_project_anchor
       pe = Periods.from_dhis2_period(params[:pe])
-      org_unit_id = params[:ou]
 
       InvoiceForProjectAnchorWorker.perform_async(
         project_anchor.id,
         pe.year,
         pe.to_quarter.quarter,
-        [org_unit_id]
+        [params[:ou]]
       )
 
       render json: { project_anchor: project_anchor.id }
