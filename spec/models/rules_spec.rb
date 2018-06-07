0c58cd11054471743539b7af45edc36900502a75
Alfred Antoine <aantoine@bluesquarehub.com>
2018-06-07 13:12:04 +0200

    fixes for decision table on a package

--- spec/models/rules_spec.rb
+++ spec/models/rules_spec.rb
@@ -216,6 +216,59 @@ RSpec.describe Rule, kind: :model do
     end
   end
 
+  describe "Package involving a decision table" do
+    let!(:activity_rule) do
+      rule = quality_package.rules.build(
+        name: "Quality PMA Budgets",
+        kind: "activity"
+      )
+
+      rule.formulas.build(
+        rule:        rule,
+        code:        :max_points,
+        expression:  "76.43",
+        description: "Max Score"
+      )
+      rule
+    end
+
+    let!(:package_rule_decision_table) do
+      rule = quality_package.rules.build(
+        name: "Quality PMA Budgets",
+        kind: "package"
+      )
+
+      rule.formulas.build(
+        rule:        rule,
+        code:        :quality_assessment_score_for_hf,
+        expression:  "SUM(%{max_points_values})",
+        description: "Quality Score"
+      )
+      rule.formulas.build(
+        rule:        rule,
+        code:        :quality_improvement_incentive_for_hf,
+        expression:  "SCORE_TABLE(quality_assessment_score_for_hf,0, 65, 0,65,75, budget_range_1,75,85, budget_range_2)",
+        description: "Quality improvement incentive"
+      )
+      rule.decision_tables.build(
+        content: ["in:groupset_code_ppa,in:groupset_code_type,out:budget_range_1,out:budget_range_2",
+                  "worldbank,fosa,1000,2000",
+                  "worldbank,hospital,3000,4000",
+                  "usaid,hospital,3000,4000"].join("\n")
+      )
+      rule
+    end
+
+    it "has validation on the activity rule when the package involves a decision table" do
+      activity_rule.valid?
+      expect(activity_rule.errors.full_messages).to eq []
+    end
+    it "has validations for package rule involving a decision table" do
+      package_rule_decision_table.valid?
+      expect(package_rule_decision_table.errors.full_messages).to eq []
+    end
+  end
+
   describe "zone packages" do
     let(:project) do
       project = build(:project, engine_version: 2)
@@ -227,6 +280,12 @@ RSpec.describe Rule, kind: :model do
 
     let(:quantity_package) do
       p = project.packages.build(kind: "zone")
+      p.states << p.project.states.select { |state| %w[Claimed].include?(state.name) }.to_a
+      p
+    end
+
+    let(:quality_package) do
+      p = project.packages.build(kind: "single")
       p.states << p.project.states.select { |state| %w[Claimed].include?(state.name) }.to_a
       p
     end
@@ -245,6 +304,7 @@ RSpec.describe Rule, kind: :model do
       )
       rule
     end
+
     let!(:package_rule) do
       rule = quantity_package.rules.build(
         name: "QUALITY score",
@@ -287,7 +347,7 @@ RSpec.describe Rule, kind: :model do
       expect(activity_rule.errors.full_messages).to eq []
     end
 
-    it "has validations for activity_rules" do
+    it "has validations for package_rules" do
       package_rule.valid?
       expect(package_rule.errors.full_messages).to eq []
     end
