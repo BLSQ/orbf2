b0f88da897d784d16c3cc422e93c4d55bc47428d
Stéphan Mestach <smestach@bluesquare.org>
2018-06-27 12:22:22 +0200

    Add link to edit the datasets in dhis2

--- app/helpers/dhis2_helper.rb
+++ app/helpers/dhis2_helper.rb
@@ -1,8 +1,15 @@
 
 module Dhis2Helper
+  def link_to_dataset(project, dhis2_id)
+    url = "#{project.dhis2_url}/dhis-web-maintenance/"\
+            "#/edit/dataSetSection/dataSet/#{dhis2_id}"
+    link_to dhis2_id, url, target: "_blank", class: "external"
+  end
+
   def link_to_data_element(project, dhis2_id)
     url = "#{project.dhis2_url}/dhis-web-maintenance/"\
             "#/edit/dataElementSection/dataElement/#{dhis2_id}"
     link_to dhis2_id, url, target: "_blank", class: "external"
   end
+
 end
