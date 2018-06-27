226f6f861a8225f6694f7b2d306f0254536b0e37
Stéphan Mestach <smestach@bluesquare.org>
2018-06-27 10:53:07 +0200

    Unused code

--- app/services/datasets/to_dhis2_datasets.rb
+++ app/services/datasets/to_dhis2_datasets.rb
@@ -11,25 +11,24 @@ module Datasets
       name = dataset_name
       dataset_info = dataset.dataset_info
       {
-        name:                name,
-        short_name:          name,
-        open_future_periods: 3,
-        data_element_ids:    dataset_info.data_elements,
-        data_set_elements:   dataset_info.data_elements.map do |de_id|
-                               {
-                                 "dataElement": {
-                                   "id": de_id
-                                 }
-                               }
-                             end,
-        organisation_units:  dataset_info.orgunits.map { |ou| { id: ou.ext_id } }
+        name:               name,
+        short_name:         name,
+        data_element_ids:   dataset_info.data_elements,
+        data_set_elements:  dataset_info.data_elements.map do |de_id|
+                              {
+                                "dataElement": {
+                                  "id": de_id
+                                }
+                              }
+                            end,
+        organisation_units: dataset_info.orgunits.map { |ou| { id: ou.ext_id } }
       }
     end
 
     def dataset_name
       [
         "ORBF",
-        dataset. payment_rule.rule.name,
+        dataset.payment_rule.rule.name,
         dataset.frequency.humanize
       ].join(" - ")
     end
