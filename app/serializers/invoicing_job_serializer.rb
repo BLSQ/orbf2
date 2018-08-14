e75d0413db8ebd4f7bee5517cd15e97582d272d0
Stéphan Mestach <smestach@bluesquare.org>
2018-08-14 08:08:49 +0200

    Move logic to backend

--- app/serializers/invoicing_job_serializer.rb
+++ app/serializers/invoicing_job_serializer.rb
@@ -3,10 +3,13 @@
 class InvoicingJobSerializer
   include FastJsonapi::ObjectSerializer
   set_key_transform :camel_lower
-  attribute :org_unit,&:orgunit_ref
+  attribute :org_unit, &:orgunit_ref
   attribute :dhis2_period
   attribute :user, &:user_ref
   attributes :processed_at, :errored_at, :duration_ms, :status, :last_error
   attribute :sidekiq_job_ref
 
+  attribute :is_alive do |job|
+    job.status != "processed" && job.status != "errored"
+  end
 end
