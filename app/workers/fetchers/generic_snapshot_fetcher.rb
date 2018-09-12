b383c10e2af41659e8387eebb80ce2f8806fc17f
Stéphan Mestach <smestach@bluesquare.org>
2018-09-12 11:33:46 +0200

    Limit args to pass in paging

--- app/workers/fetchers/generic_snapshot_fetcher.rb
+++ app/workers/fetchers/generic_snapshot_fetcher.rb
@@ -1,6 +1,24 @@
 # frozen_string_literal: true
 
 module Fetchers
+  class Progress
+    attr_accessor :kind, :page, :page_size, :page_count, :filter, :start, :fields
+
+    def initialize(options)
+      @start = Time.current
+      @kind = options.fetch(:kind)
+      @page = 1
+      @page_size = options.fetch(:page_size)
+      @filter = options.fetch(:filter)
+      @fields = options.fetch(:fields)
+    end
+
+    def log_progress(paged_data)
+      puts "#{Time.current} \t Processed #{kind} page #{page} of #{page_count || '?'} "\
+            " (Size: #{paged_data.size}, total time : #{Time.current - start}) #{filter} #{fields}"
+    end
+  end
+
   class GenericSnapshotFetcher
     attr_reader :fields, :start
 
@@ -11,14 +29,14 @@ module Fetchers
     def fetch_data(project, kind, filter: nil, page_size: 5000)
       data = []
       begin
-        @start = Time.new
         dhis2 = project.dhis2_connection
-        page_count= nil
-        paged_data = fetch_paged_data(dhis2, data, kind, 1, page_size, page_count, filter)
-        page_count = paged_data.pager.page_count
-        if page_count > 1
-          (2..page_count).each do |page|
-            fetch_paged_data(data, kind, page, page_size, page_count, filter)
+        progress = Progress.new(kind: kind, page_size: page_size, filter: filter, fields: fields)
+        paged_data = fetch_paged_data(dhis2, data, progress)
+        progress.page_count = paged_data.pager.page_count
+        if progress.page_count > 1
+          (2..progress.page_count).each do |page|
+            progress.page = page
+            fetch_paged_data(dhis2, data, progress)
           end
         end
       rescue RestClient::Exception => e
@@ -28,22 +46,17 @@ module Fetchers
       data
     end
 
-    def fetch_paged_data(dhis2, data, kind, page, page_size, page_count, filter)
-      page = nil if page == 1
-      paged_data = dhis2.send(kind).list(
-        page:      page,
+    def fetch_paged_data(dhis2, data, progress)
+      actual_page = progress.page == 1 ? nil : progress.page
+      paged_data = dhis2.send(progress.kind).list(
+        page:      actual_page,
         fields:    fields,
-        page_size: page_size,
-        filter:    filter
+        page_size: progress.page_size,
+        filter:    progress.filter
       )
-      log_progress(page, page_count, paged_data, filter)
+      progress.log_progress(paged_data)
       data.push(*paged_data)
       paged_data
-    end
-
-    def log_progress(page, page_count, paged_data, filter)
-      puts "#{Time.new} \t Processed page #{page} of #{page_count} "\
-            " (Size: #{paged_data.size}, total time : #{Time.new - start}) #{filter} #{fields}"
     end
   end
 end
