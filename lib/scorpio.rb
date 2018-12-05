14349f2f5ba220cc70673efdc01b09c83ccb4700
pjaspers <pjaspers@bluesquarehub.com>
2018-12-05 16:57:23 +0100

    Fix rubocop issues

--- lib/scorpio.rb
+++ lib/scorpio.rb
@@ -1,7 +1,11 @@
-module Scorpio
+# frozen_string_literal: true
 
-  # Development or QA environemnts will return true
+module Scorpio
+  # Development or QA environments will return true
+  # rubocop:disable Naming/PredicateName
+  # In this case, I think the `is_` actually provides some value
   def self.is_dev?
-    Rails.env.development? || ENV['ORBF_STAGING']
+    Rails.env.development? || ENV["ORBF_STAGING"]
   end
+  # rubocop:enable Naming/PredicateName
 end
