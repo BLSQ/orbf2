ba0e8ac2c297ea143112061e5877f2fd9b870623
pjaspers <pjaspers@bluesquarehub.com>
2019-04-02 11:06:29 +0200

    Add missing annotation

--- spec/factories/invoicing_jobs.rb
+++ spec/factories/invoicing_jobs.rb
@@ -1,3 +1,32 @@
+# == Schema Information
+#
+# Table name: invoicing_jobs
+#
+#  id                :bigint(8)        not null, primary key
+#  dhis2_period      :string           not null
+#  duration_ms       :integer
+#  errored_at        :datetime
+#  last_error        :string
+#  orgunit_ref       :string           not null
+#  processed_at      :datetime
+#  sidekiq_job_ref   :string
+#  status            :string
+#  type              :string           default("InvoicingJob")
+#  user_ref          :string
+#  created_at        :datetime         not null
+#  updated_at        :datetime         not null
+#  project_anchor_id :integer          not null
+#
+# Indexes
+#
+#  index_invoicing_jobs_on_anchor_ou_period   (project_anchor_id,orgunit_ref,dhis2_period,type) UNIQUE
+#  index_invoicing_jobs_on_project_anchor_id  (project_anchor_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (project_anchor_id => project_anchors.id)
+#
+
 FactoryBot.define do
   factory :invoicing_job do
     dhis2_period { "2018Q1" }
