04ccee0da826f2fe996a6641147978ed8ea5517c
pjaspers <pjaspers@bluesquarehub.com>
2019-04-03 16:02:41 +0200

    Do the mapping only once

--- app/javascript/packs/invoice_list.jsx
+++ app/javascript/packs/invoice_list.jsx
@@ -1,80 +1,89 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import PropTypes from "prop-types";
 import humanize from "string-humanize";
 import MultiSelectDropdown from "./multi_select_dropdown";
 import { Invoice } from "./invoice";
 
+const mapPeriods = invoices => {
+  return [...new Set(invoices.map(invoice => invoice.period))];
+};
+
+const mapPackages = invoices => {
+  return [...new Set(invoices.map(invoice => humanize(invoice.code)))];
+};
+
+const mapOrgunits = invoices => {
+  return [...new Set(invoices.map(invoice => invoice.orgunit_ext_id))];
+};
+
 class InvoiceList extends React.Component {
   constructor(props) {
     super(props);
+    const periods = mapPeriods(props.invoices);
+    const packages = mapPackages(props.invoices);
+    const orgunits = mapOrgunits(props.invoices);
+    this.state = {
+      periods,
+      allPeriods: periods,
+      packages,
+      allPackages: packages,
+      orgUnits: [orgunits[0]],
+      allOrgUnits: orgunits,
+    };
   }
-
-  state = {
-    periods: [],
-    orgUnits: [],
-    packages: [],
-  };
 
   periodsChanged = periods => {
     this.setState({ periods });
   };
 
-  packagesChanged = periods => {
-    this.setState({ packages: periods });
+  packagesChanged = packages => {
+    this.setState({ packages });
   };
 
-  orgUnitsChanged = units => {
-    this.setState({ orgUnits: units });
-  };
-
-  allPeriods = invoices => {
-    return [...new Set(invoices.map(invoice => invoice.period))];
-  };
-
-  allPackages = invoices => {
-    return [...new Set(invoices.map(invoice => humanize(invoice.code)))];
-  };
-
-  allOrgunits = invoices => {
-    return [...new Set(invoices.map(invoice => invoice.orgunit_ext_id))];
+  orgUnitsChanged = orgUnits => {
+    this.setState({ orgUnits });
   };
 
   render() {
-    const filteredInvoices = this.props.invoices.filter(invoice => {
+    const {
+      allPeriods,
+      periods,
+      allPackages,
+      packages,
+      allOrgUnits,
+      orgUnits,
+    } = this.state;
+
+    const { invoices } = this.props;
+
+    const filteredInvoices = invoices.filter(invoice => {
       return (
-        this.state.periods.includes(invoice.period) &&
-        this.state.packages.includes(humanize(invoice.code)) &&
-        this.state.orgUnits.includes(invoice.orgunit_ext_id)
+        periods.includes(invoice.period) &&
+        packages.includes(humanize(invoice.code)) &&
+        orgUnits.includes(invoice.orgunit_ext_id)
       );
     });
-    const allPeriods = this.allPeriods(this.props.invoices);
-    const allPackages = this.allPackages(this.props.invoices);
-    const allOrgUnits = this.allOrgunits(this.props.invoices);
-    const selectedPeriods = this.state.periods;
-    const selectedPackages = this.state.packages;
 
     return [
       <MultiSelectDropdown
         name="Periods"
         names={allPeriods}
-        selected={selectedPeriods}
+        selected={periods}
         optionsChanged={this.periodsChanged}
-        key={"periods"}
+        key="periods"
       />,
       <MultiSelectDropdown
         name="Org Units"
         names={allOrgUnits}
-        selected={this.state.orgUnits}
+        selected={orgUnits}
         optionsChanged={this.orgUnitsChanged}
-        key={"orgUnits"}
+        key="orgUnits"
       />,
       <MultiSelectDropdown
         name="Packages"
         names={allPackages}
-        selected={selectedPackages}
+        selected={packages}
         optionsChanged={this.packagesChanged}
-        key={"packages"}
+        key="packages"
       />,
       filteredInvoices.map((invoice, i) => {
         const key = [invoice.orgunit_ext_id, invoice.period, invoice.code].join(
