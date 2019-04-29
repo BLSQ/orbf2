a145222edb45662e3024db765b6e3bb17e4f1dc4
Stéphan Mestach <smestach@bluesquare.org>
2019-04-29 21:03:37 +0200

    Display explanation steps for indicators too

--- app/javascript/packs/components/invoice/explanation_row.jsx
+++ app/javascript/packs/components/invoice/explanation_row.jsx
@@ -3,6 +3,10 @@ import React from "react";
 
 const backgroundColor = "rgb(253, 250, 249)";
 
+const ExplanationStep = props => (
+  <pre style={{ whiteSpace: "pre-wrap" }}>{props.children}</pre>
+);
+
 const ExplanationRow = function(props) {
   return (
     <tr key={`explanation-${props.header}`} style={{ backgroundColor }}>
@@ -13,34 +17,36 @@ const ExplanationRow = function(props) {
             <dd>{humanize(props.header)}</dd>
             <dt>Key</dt>
             <dd>{props.rowData.key}</dd>
-            {props.rowData.dhis2_data_element && [
-              <dt>Expression</dt>,
-              <dd>{props.rowData.expression}</dd>,
-            ]}
+
             {props.rowData.state && [
               <dt key="mapping-label">Mapping</dt>,
               <dd key="mapping-value">
-                {props.rowData.state.kind} - {props.rowData.state.ext_id || "" }
+                {props.rowData.state.kind} - {props.rowData.state.ext_id || ""}
               </dd>,
             ]}
             {props.rowData.dhis2_data_element && [
-              <dt>DHIS2-element</dt>,
-              <dd>{props.rowData.dhis2_data_element}</dd>,
+              <dt key="de-label">DHIS2-element</dt>,
+              <dd key="de-value">{props.rowData.dhis2_data_element}</dd>,
             ]}
           </dl>
 
-          {props.rowData.expression && props.rowData.dhis2_data_element && (
+          {props.rowData.instantiated_expression && (
             <>
               <h5>Step by step explanations:</h5>
-              <pre style={{ whiteSpace: "pre-wrap" }}>
+              {props.rowData.expression && (
+                <ExplanationStep>
+                  {`${props.header} = ${props.rowData.expression}`}
+                </ExplanationStep>
+              )}
+              <ExplanationStep>
                 {`${props.header} = ${props.rowData.instantiated_expression}`}
-              </pre>
-              <pre style={{ whiteSpace: "pre-wrap" }}>
+              </ExplanationStep>
+              <ExplanationStep>
                 {`${props.header} = ${props.rowData.substituted}`}
-              </pre>
-              <pre style={{ whiteSpace: "pre-wrap" }}>
+              </ExplanationStep>
+              <ExplanationStep>
                 {`${props.header} = ${props.rowData.solution}`}
-              </pre>
+              </ExplanationStep>
             </>
           )}
         </div>
