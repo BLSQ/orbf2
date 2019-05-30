a00d7544e904c790c436b77684204a54616063a8
mestachs <mestachs>
2019-05-31 00:18:19 +0200

    More cleanup

--- app/workers/invoice_for_project_anchor_worker.rb
+++ app/workers/invoice_for_project_anchor_worker.rb
@@ -22,28 +22,14 @@ class InvoiceForProjectAnchorWorker
 
       project = project_anchor.projects.for_date(request.end_date_as_date) || project_anchor.latest_draft
       request.engine_version = project.engine_version
-      if project.new_engine? && selected_org_unit_ids.size == 1
-        options = Invoicing::InvoicingOptions.new(
-          publish_to_dhis2:       true,
-          force_project_id:       nil,
-          allow_fresh_dhis2_data: false
-        )
-        request.entity = selected_org_unit_ids.first
-        invoice_entity = Invoicing::InvoiceEntity.new(project_anchor, request, options)
-        invoice_entity.call
-      else
-        raise "TODO : no more supported"
-      end
+      options = Invoicing::InvoicingOptions.new(
+        publish_to_dhis2:       true,
+        force_project_id:       nil,
+        allow_fresh_dhis2_data: false
+      )
+      request.entity = selected_org_unit_ids.first
+      invoice_entity = Invoicing::InvoiceEntity.new(project_anchor, request, options)
+      invoice_entity.call
     end
-  end
-
-  def organisation_units(project_anchor, request)
-    request.quarter_dates.map do |quarter_date|
-      pyramid = project_anchor.nearest_pyramid_for(quarter_date)
-      project = project_anchor.projects.for_date(quarter_date) || project_anchor.latest_draft
-      contracted_entities = pyramid.org_units_in_all_groups([project.entity_group.external_reference])
-      Rails.logger.info "quarter_date #{quarter_date.year}-#{quarter_date.month} => projects #{project.status} #{project.id} => #{contracted_entities.size} (#{project.entity_group.external_reference})"
-      contracted_entities.map(&:id)
-    end.flatten.uniq.sort
   end
 end
