e5d46e9a06813842575df578c23202ae83a9abe6
mestachs <mestachs>
2019-05-30 23:45:02 +0200

    More spring cleanup

--- app/services/invoicing/payment_result.rb
+++
@@ -1,9 +0,0 @@
-module Invoicing
-  class PaymentResult < Struct.new(:payment_rule, :solution, :variables)
-    def to_s
-      "#{payment_rule.name}  #{solution}"
-    end
-
-    delegate :to_json, to: :to_h
-  end
-end
--- app/services/mock_project_finder.rb
+++
@@ -1,5 +0,0 @@
-class MockProjectFinder
-  def find_project(_date)
-    ProjectFactory.new.build
-  end
-end
