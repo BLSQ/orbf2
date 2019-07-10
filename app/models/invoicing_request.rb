940de784cbe42af98fbb3cdc4aab205ea287a118
Stéphan Mestach <smestach@bluesquare.org>
2019-07-10 13:50:05 +0200

    Fix recorded test by defaulting origin attribute

--- app/models/invoicing_request.rb
+++ app/models/invoicing_request.rb
@@ -11,6 +11,7 @@ class InvoicingRequest
   def initialize(*)
     super
     @mocked_data ||= [] if mock_values?
+    @mocked_data.each { |v| v["origin"] ||= "dataValueSets" }
   end
 
   def start_date_as_date
