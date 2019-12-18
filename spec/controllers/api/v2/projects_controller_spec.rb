42ca397f28255a2c412ce81c4d6f2a032d5e21d4
pjaspers <pjaspers@bluesquarehub.com>
2019-12-18 22:35:06 +0100

    Add Project serializer
    More resilient spec
    
    Expose periods

--- spec/controllers/api/v2/projects_controller_spec.rb
+++ spec/controllers/api/v2/projects_controller_spec.rb
@@ -5,7 +5,6 @@ RSpec.describe Api::V2::ProjectsController, type: :controller do
 
   let(:program) { create :program }
   let(:token) { "123456789" }
-  let(:project_anchor) { create :project_anchor, token: token, program: program }
   let!(:project) do
     project = full_project
     project.project_anchor.update_attributes(token: token)
@@ -16,11 +15,20 @@ RSpec.describe Api::V2::ProjectsController, type: :controller do
   describe "#show" do
     it "returns project" do
       request.headers["Accept"] = "application/vnd.api+json;version=2"
-      request.headers["X-Token"] = project_anchor.token
+      request.headers["X-Token"] = project.project_anchor.token
       get(:show, params: {})
       resp = JSON.parse(response.body)
 
       expect(resp["data"]["id"]).to eq(project.id.to_s)
+    end
+
+    it "returns periods" do
+      request.headers["Accept"] = "application/vnd.api+json;version=2"
+      request.headers["X-Token"] = project.project_anchor.token
+      get(:show, params: {})
+      resp = JSON.parse(response.body)
+
+      expect(resp["data"]["attributes"]["periods"]).to_not be_empty
     end
   end
 end
