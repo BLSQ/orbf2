e7f39b46da1ee4722efe1a1073b70a192ea60c49
pjaspers <pjaspers@bluesquarehub.com>
2019-12-18 15:55:19 +0100

    Also add dhis2 url
    Use correct attribute

--- app/serializers/v2/project_anchor_serializer.rb
+++ app/serializers/v2/project_anchor_serializer.rb
@@ -3,6 +3,10 @@ class V2::ProjectAnchorSerializer
   set_key_transform :camel_lower
   set_type :project
 
+  attribute :dhis2_url do |project_anchor|
+    project_anchor.project&.dhis2_url
+  end
+
   attribute :created_at
   attribute :updated_at
   attribute :cycle do |project_anchor|
@@ -15,6 +19,11 @@ class V2::ProjectAnchorSerializer
   attribute :code do |project_anchor|
     project_anchor.program&.code
   end
+
+  has_many :inputs do |project_anchor|
+    project_anchor.project&.states
+  end
+
   has_many :simulations do |project_anchor|
     project_anchor.invoicing_simulation_jobs
   end
