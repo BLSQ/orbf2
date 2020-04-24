f133fa2b7feab811a88771a108fc03f47c07e442
pjaspers <pjaspers@bluesquarehub.com>
2020-04-24 16:47:13 +0200

    Also allow the find to add fields

--- app/services/autocomplete/dhis2.rb
+++ app/services/autocomplete/dhis2.rb
@@ -19,7 +19,9 @@ module Autocomplete
     # Skip default
     def search(name, kind: "data_elements", limit: 20, fields: [:id, :display_name, :code])
       to_pluck = fields.map do |key|
-        "element->'table'->>'#{key}'"
+        parts = key.to_s.split("__")
+        k = parts.map{|part| "'#{part}'"}.join("->")
+        Arel.sql("element->'table'->#{k}")
       end
 
       # SELECT  "dhis2_snapshots".* FROM (
@@ -36,38 +38,37 @@ module Autocomplete
       max_id_rel = Dhis2Snapshot.where(kind: kind).where(project_anchor_id: @project_anchor).select("max(id)")
       unnest_elements_rel = Dhis2Snapshot.select("jsonb_array_elements(content) as element").from(Dhis2Snapshot.where(id: max_id_rel))
 
+      result = Struct.new(*fields)
       Dhis2Snapshot.from(unnest_elements_rel).
         where("element->'table'->>'display_name' ILIKE ?", "%#{name}%").
         limit(limit).
-        pluck(*to_pluck).map do |id, display_name, code,  _category_option_combos|
-        Result.new(id: id, code: code, display_name: display_name, category_combo: nil)
+        pluck(*to_pluck).map do |a|
+        # r = fields.zip(a).to_h
+        # Result.new(id: r[:id], code: r[:code], display_name: r[:display_name], category_combo: r)
+        result.new(*a)
       end
     end
 
-    def find(id, kind: "data_elements")
-      Dhis2Snapshot.connection.select_all(
-        ActiveRecord::Base.send(
-          :sanitize_sql_array,
-          [FIND_QUERY, kind, @project_anchor.id, id]
-        )
-      ).to_hash.map { |e| Result.new(id: e["id"], code: e["code"], display_name: e["display_name"]) }
-    end
+    def find(id, kind: "data_elements", fields: [:id, :display_name, :code])
+      to_pluck = fields.map do |key|
+        parts = key.to_s.split("__")
+        k = parts.map{|part| "'#{part}'"}.join("->")
+        Arel.sql("element->'table'->#{k}")
+      end
 
-    FIND_QUERY = "select * from (
-      select
-        (element->'table'->>'id')::text as id ,
-            (element->'table'->>'display_name')::text as display_name,
-           (element->'table'->>'code')::text as code
-        from (
-             select jsonb_array_elements(content) as element
-             from dhis2_snapshots
-             where
-             id=(
-                SELECT max(id) FROM dhis2_snapshots
-                WHERE kind= ? AND project_anchor_id = ?)
-             )
-         as elements
-      ) as dataelements
-       where id = ?"
+      max_id_rel = Dhis2Snapshot.where(kind: kind).where(project_anchor_id: @project_anchor).select("max(id)")
+      unnest_elements_rel = Dhis2Snapshot.select("jsonb_array_elements(content) as element").from(Dhis2Snapshot.where(id: max_id_rel))
+
+      limit = 20
+      result = Struct.new(*fields)
+      Dhis2Snapshot.from(unnest_elements_rel).
+        where("element->'table'->>'id' IN (?)", Array.wrap(id)).
+        limit(limit).
+        pluck(*to_pluck).map do |a|
+        # r = fields.zip(a).to_h
+        # Result.new(id: r[:id], code: r[:code], display_name: r[:display_name], category_combo: r)
+        result.new(*a)
+      end
+    end
   end
 end
