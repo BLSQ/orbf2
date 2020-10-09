ecefa9d586ac91a0b653bdc3b05b3c940824dafc
Stéphan Mestach <smestach@bluesquare.org>
2020-10-09 10:09:11 +0200

    Remove duplicated code

--- app/workers/synchros/v1/data_element_groups.rb
+++ app/workers/synchros/v1/data_element_groups.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Synchros::V1::DataElementGroups
+class Synchros::V1::DataElementGroups < Synchros::Base
   def synchronize(package)
     Rails.logger.info "********** Synchronizing #{package.name} (#{package.id}) - activities #{package.activities.size}"
     @indicators ||= package.project.dhis2_connection.indicators.list(fields: ":all", page_size: 50_000)
@@ -143,15 +143,4 @@ class Synchros::V1::DataElementGroups
     raise "Failed to create dataset #{ds} #{e.message} with #{package.project.dhis2_url} #{e.response.body}"
   end
 
-  def indicators_data_element_references(indicators, activity_states)
-    indicator_references = Set.new(activity_states.select(&:kind_indicator?).select(&:origin_data_value_sets?).map(&:external_reference))
-    parser_klazz = Orbf::RulesEngine::IndicatorExpressionParser
-    dataelement_references = indicators.select { |i| indicator_references.include?(i.id) }
-                                       .map { |indicator| parser_klazz.parse_expression(indicator.numerator) }
-                                       .flatten
-                                       .compact
-                                       .map(&:data_element)
-    Rails.logger.info "Adding indicator's data elements #{indicator_references.to_a} => #{dataelement_references}"
-    dataelement_references
-  end
 end
--- app/workers/synchros/v2/data_element_groups.rb
+++ app/workers/synchros/v2/data_element_groups.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-class Synchros::V2::DataElementGroups
+class Synchros::V2::DataElementGroups < Synchros::Base
   def synchronize(package)
     Rails.logger.info "********** Synchronizing #{package.name} (#{package.id}) - activities #{package.activities.size}"
     @indicators ||= package.project.dhis2_connection.indicators.list(fields: "id,name,numerator", page_size: 50_000)
@@ -61,17 +61,5 @@ class Synchros::V2::DataElementGroups
 
       return created_deg
     end
-  end
-
-  def indicators_data_element_references(indicators, activity_states)
-    indicator_references = Set.new(activity_states.select(&:kind_indicator?).select(&:origin_data_value_sets?).map(&:external_reference))
-    parser_klazz = Orbf::RulesEngine::IndicatorExpressionParser
-    dataelement_references = indicators.select { |i| indicator_references.include?(i.id) }
-                                       .map { |indicator| parser_klazz.parse_expression(indicator.numerator) }
-                                       .flatten
-                                       .compact
-                                       .map(&:data_element)
-    Rails.logger.info "Adding indicator's data elements #{indicator_references.to_a} => #{dataelement_references}"
-    dataelement_references
   end
 end
