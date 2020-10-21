599257010488597393eda283ffc4fe15ffdaec93
Stéphan Mestach <smestach@bluesquare.org>
2020-10-21 21:59:58 +0200

    Add api to load fully a project

--- app/serializers/v2/project_anchor_serializer.rb
+++ app/serializers/v2/project_anchor_serializer.rb
@@ -1,11 +1,11 @@
 # frozen_string_literal: true
 
 class V2::ProjectAnchorSerializer < V2::BaseSerializer
-  attribute :dhis2_url do |project_anchor|
-    project_anchor.project&.dhis2_url
+  attribute :dhis2_url do |project|
+    project&.dhis2_url
   end
-  attribute :periods do |project_anchor|
-    project = project_anchor.project
+  attribute :periods do |project|
+
     if project
       start_year = 2016
       end_year = project.calendar.from_iso(DateTime.now).year + 1
@@ -16,22 +16,30 @@ class V2::ProjectAnchorSerializer < V2::BaseSerializer
   end
   attribute :created_at
   attribute :updated_at
-  attribute :cycle do |project_anchor|
-    project_anchor.project&.cycle
+  attribute :cycle do |project|
+    project&.cycle
   end
-  attribute :name do |project_anchor|
-    project_anchor.project&.name
-  end
-
-  attribute :code do |project_anchor|
-    project_anchor.program&.code
+  attribute :name do |project|
+    project&.name
   end
 
-  has_many :inputs do |project_anchor|
-    project_anchor.project&.states
+  attribute :code do |project|
+    project&.project_anchor&.program&.code
   end
 
-  has_many :simulations do |project_anchor|
-    project_anchor.invoicing_simulation_jobs
+  has_many :inputs do |project|
+    project&.states
+  end
+
+  has_many :sets, serializer: V2::PackageSerializer, record_type: "set"  do |project|
+    project&.packages
+  end
+
+  has_many :compounds, serializer: V2::PaymentRuleSerializer, record_type: "compound"  do |project|
+    project.project_anchor.project&.payment_rules
+  end
+
+  has_many :simulations do |project|
+    project.project_anchor.invoicing_simulation_jobs
   end
 end
