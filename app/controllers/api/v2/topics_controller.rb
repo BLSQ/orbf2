7276b6608dd31a4f322691991f6e289430f20078
Stéphan Mestach <smestach@bluesquare.org>
2021-01-16 17:25:43 +0100

    Handle sets relationships

--- app/controllers/api/v2/topics_controller.rb
+++ app/controllers/api/v2/topics_controller.rb
@@ -8,12 +8,17 @@ module Api::V2
       topic = current_project.activities.create!(topic_attributes)
       # make stable id visible
       topic.reload
+
+      synchronize_relationships(topic)
+
       render json: serializer_class.new(topic).serialized_json
     end
 
     def update
       topic = current_project.activities.find(params[:id])
       topic.update!(topic_attributes)
+
+      synchronize_relationships(topic)
 
       render json: serializer_class.new(topic).serialized_json
     end
@@ -24,6 +29,28 @@ module Api::V2
     end
 
     private
+
+    def synchronize_relationships(topic)
+      # handle optional set relationships
+      return unless params[:data]
+      return unless params[:data][:relationships]
+      return unless params[:data][:relationships][:sets]
+
+      package_ids = params[:data][:relationships][:sets].map { |hesabuset| hesabuset["id"] }
+      
+      current_project.packages.each do |package|
+        activity_package = package.activity_packages.where(activity_id: topic.id).first
+
+        if package_ids.include?(package.id.to_s) && activity_package.nil?
+          # puts "adding #{topic} to package #{package}"
+           package.activities << topic 
+        end
+        if activity_package && !package_ids.include?(package.id)
+          #puts "removing #{topic} from package #{package}"
+          activity_package.destroy 
+        end
+      end
+    end
 
     def current_project
       current_project_anchor.project
--- spec/controllers/api/v2/topics_controller_spec.rb
+++ spec/controllers/api/v2/topics_controller_spec.rb
@@ -3,10 +3,15 @@
 require "rails_helper"
 
 RSpec.describe Api::V2::TopicsController, type: :controller do
+  def ensure_json(payload)
+    JSON.parse(JSON.generate(payload))
+  end
+
   let(:token) { "123456789" }
   let(:program) { create :program }
   let(:project_anchor) { create :project_anchor, token: token, program: program }
   let(:project) { create :project, project_anchor: project_anchor }
+  let(:package) { project.packages.create!(name: "HF quantity", kind: "single", frequency: "monthly", data_element_group_ext_ref: "healthfacilities") }
 
   def authenticated
     request.headers["Accept"] = "application/vnd.api+json;version=2"
@@ -19,12 +24,14 @@ RSpec.describe Api::V2::TopicsController, type: :controller do
       authenticated
     end
     let(:payload) {
-      { data: {
-        attributes: {
-          name:      "Topic very long name",
-          shortName: "Topic"
+      ensure_json(
+        data: {
+          attributes: {
+            name:      "Topic very long name",
+            shortName: "Topic"
+          }
         }
-      } }
+      )
     }
     it "create a topic" do
       post(:create, params: payload)
@@ -39,6 +46,27 @@ RSpec.describe Api::V2::TopicsController, type: :controller do
       expect(attributes["shortName"]).to eq("Topic")
 
       expect(attributes["stableId"]).not_to be_nil
+    end
+
+    describe "on existing set" do
+      it "create a topic and put it in an existing set" do
+        payload["data"]["relationships"] = { "sets" => [{ "type" => "set", "id" => package.id.to_s }] }
+        post(:create, params: payload)
+
+        resp = JSON.parse(response.body)
+
+        expect(resp["data"]["type"]).to eq("topic")
+
+        attributes = resp["data"]["attributes"]
+        expect(attributes["code"]).to eq("topic_very_long_name")
+        expect(attributes["name"]).to eq("Topic very long name")
+        expect(attributes["shortName"]).to eq("Topic")
+
+        expect(attributes["stableId"]).not_to be_nil
+
+        package.reload
+        expect(package.activities.first.id.to_s).to eq(resp["data"]["id"])
+      end
     end
 
     it "create a topic handles validation errors" do
@@ -61,7 +89,7 @@ RSpec.describe Api::V2::TopicsController, type: :controller do
       authenticated
     end
     let(:payload) {
-      {
+      ensure_json(
         id:   activity.id,
         data: {
           attributes: {
@@ -70,7 +98,7 @@ RSpec.describe Api::V2::TopicsController, type: :controller do
             code:      "med_01"
           }
         }
-      }
+      )
     }
     let(:activity) {
       project.activities.create!(name: "demo")
@@ -94,8 +122,36 @@ RSpec.describe Api::V2::TopicsController, type: :controller do
       expect(attributes["stableId"]).to eq(activity.stable_id)
     end
 
+    describe "on existing set" do
+      let(:package2) { project.packages.create!(name: "HF quality", kind: "single", frequency: "quarterly", data_element_group_ext_ref: "healthfacilities") }
+
+      it "update a topic" do
+        package2.activities << activity
+
+        activity.reload
+        payload["data"]["relationships"] = { "sets" => [{ "type" => "set", "id" => package.id.to_s }] }
+
+        put(:update, params: payload)
+
+        resp = JSON.parse(response.body)
+
+        expect(resp["data"]["type"]).to eq("topic")
+
+        attributes = resp["data"]["attributes"]
+
+        expect(attributes["code"]).to eq("med_01")
+        expect(attributes["name"]).to eq("Topic very long name new")
+        expect(attributes["shortName"]).to eq("Topic new")
+
+        expect(attributes["stableId"]).to eq(activity.stable_id)
+
+        package2.reload
+        expect(package.activities.map(&:code)).not_to include(activity.code)
+      end
+    end
+
     it "update a topic handles validation errors" do
-      payload[:data][:attributes][:shortName] = "super very veryveryveryveryveryveryveryveryveryveryveryveryvery long"
+      payload["data"]["attributes"]["shortName"] = "super very veryveryveryveryveryveryveryveryveryveryveryveryvery long"
       put(:update, params: payload)
 
       resp = JSON.parse(response.body)
