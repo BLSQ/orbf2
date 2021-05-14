a0405a8a2ba5997c2a54297b0e4552d1d5e713c8
Stéphan Mestach <smestach@bluesquare.org>
2021-05-14 14:53:07 +0200

    Render out of condition

--- app/controllers/setup/project_rules_controller.rb
+++ app/controllers/setup/project_rules_controller.rb
@@ -51,11 +51,11 @@ class Setup::ProjectRulesController < PrivateController
     Rails.logger.info project_rule.errors.full_messages
     if project_rule.save
       flash[:notice] = "Rule updated !"
-      render action: "edit"
     else
       project_rule.rule.formulas.build if project_rule.rule.formulas.empty?
-      render action: "edit"
     end
+
+    render action: "edit"
   end
 
   private
