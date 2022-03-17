fbd946ec9d9ee7c867ed26c75a2fb527d1c37626
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-03-17 17:02:39 +0100

    testing edge cases in oauth callback

--- spec/controllers/oauth/oauth_controller_spec.rb
+++ spec/controllers/oauth/oauth_controller_spec.rb
@@ -3,31 +3,99 @@
 require "rails_helper"
 
 RSpec.describe Oauth::OauthController, type: :controller do
-  describe "valid information" do
-    let(:program) { FactoryBot.create(:program, oauth_client_id: "test-program", oauth_client_secret: "1234abcd") }
-    describe "valid code" do
+  describe "#callback" do
+    let!(:program) { 
+      program = FactoryBot.create(:program, oauth_client_id: "test-program", oauth_client_secret: "1234abcd")
+      FactoryBot.create(:project, project_anchor: program.build_project_anchor)
+      program
+    }
+    
+    let(:user) { 
+      FactoryBot.create(:user, dhis2_user_ref: "dhis2userref", program_id: program.id) 
+    }
+
+    let(:code) { "123456" }
+
+    let(:access_token) { "randomaccesstoken" } 
+
+    describe "valid information" do
       it "should redirect to home" do
-        FactoryBot.create(:project, project_anchor: program.build_project_anchor)
-        dhis2_user_ref = "dhis2userref"
-        FactoryBot.create(:user, dhis2_user_ref: dhis2_user_ref)
-        
-        url_post = program.project_anchor.project.dhis2_url + "/uaa/oauth/token"
-        code = "123456"
-        access_token = "randomaccesstoken"
-        stub_request(:post, url_post).
+        stub_request(:post, program.project_anchor.project.dhis2_url + "/uaa/oauth/token").
           with(body: {"code"=>code, "grant_type"=>"authorization_code"}).
           to_return(status: 200, body: "{\"access_token\":\"#{access_token}\",\"token_type\":\"bearer\",\"expires_in\":24526,\"scope\":\"ALL\"}")
-        
-        url_get = program.project_anchor.project.dhis2_url + "/api/me"
-        stub_request(:get, url_get).
+
+        stub_request(:get, program.project_anchor.project.dhis2_url + "/api/me").
           with(headers: {"Authorization"=>"Bearer #{access_token}"}).
-          to_return(status: 200, body: "{\"userCredentials\":{\"id\":\"#{dhis2_user_ref}\"}}")
+          to_return(status: 200, body: "{\"id\":\"#{user.dhis2_user_ref}\"}")
 
         @request.env["devise.mapping"] = Devise.mappings[:user]
         get :callback, params: { program_id: program.id, code: code }
 
         expect(response.redirect?).to eq(true)
         expect(response).to redirect_to("/")
+      end
+    end
+
+    describe "invalid information" do
+      describe "invalid code" do
+        it "should redirect to users sign-in" do
+          stub_request(:post, program.project_anchor.project.dhis2_url + "/uaa/oauth/token").
+            with(body: {"code"=>code, "grant_type"=>"authorization_code"}).
+            to_return(status: 400, body: "{\"error\":\"invalid_grant\",\"error_description\":\"Invalid authorization code: #{code}\"}")
+   
+          @request.env["devise.mapping"] = Devise.mappings[:user]
+
+          get :callback, params: { program_id: program.id, code: "123456" }
+
+          expect(response).to redirect_to("/users/sign_in")
+        end
+      end
+
+      describe "user not found based on dhis2_user_ref" do
+        it "should redirect to users sign-in" do
+          stub_request(:post, program.project_anchor.project.dhis2_url + "/uaa/oauth/token").
+          with(body: {"code"=>code, "grant_type"=>"authorization_code"}).
+          to_return(status: 200, body: "{\"access_token\":\"#{access_token}\",\"token_type\":\"bearer\",\"expires_in\":24526,\"scope\":\"ALL\"}")
+
+          stub_request(:get, program.project_anchor.project.dhis2_url + "/api/me").
+            with(headers: {"Authorization"=>"Bearer #{access_token}"}).
+            to_return(status: 200, body: "{\"id\":\"unknownuserref\"}")
+
+          @request.env["devise.mapping"] = Devise.mappings[:user]
+          get :callback, params: { program_id: program.id, code: code }
+
+          expect(response).to redirect_to("/users/sign_in")
+        end
+      end
+
+      describe "bad response from DHIS2 when returning access token" do
+        it "should redirect to users sign-in" do
+          stub_request(:post, program.project_anchor.project.dhis2_url + "/uaa/oauth/token").
+          with(body: {"code"=>code, "grant_type"=>"authorization_code"}).
+          to_return(status: 200, body: "<p>DHIS2 timeout</p>")
+
+          @request.env["devise.mapping"] = Devise.mappings[:user]
+          get :callback, params: { program_id: program.id, code: code }
+
+          expect(response).to redirect_to("/users/sign_in")
+        end
+      end
+
+      describe "bad response from DHIS2 when returning user info" do
+        it "should redirect to users sign-in" do
+          stub_request(:post, program.project_anchor.project.dhis2_url + "/uaa/oauth/token").
+          with(body: {"code"=>code, "grant_type"=>"authorization_code"}).
+          to_return(status: 200, body: "{\"access_token\":\"#{access_token}\",\"token_type\":\"bearer\",\"expires_in\":24526,\"scope\":\"ALL\"}")
+
+          stub_request(:get, program.project_anchor.project.dhis2_url + "/api/me").
+            with(headers: {"Authorization"=>"Bearer #{access_token}"}).
+            to_return(status: 200, body: "<p>DHIS2 timeout</p>")
+
+          @request.env["devise.mapping"] = Devise.mappings[:user]
+          get :callback, params: { program_id: program.id, code: code }
+
+          expect(response).to redirect_to("/users/sign_in")
+        end
       end
     end
   end
