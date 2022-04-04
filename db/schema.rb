c58dd47b286321100d62cdccc10d427993573529
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-04-04 17:31:22 +0200

    db changes to add name, comment, source_url to decision_tables

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_03_16_120758) do
+ActiveRecord::Schema.define(version: 2022_04_04_170152) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "pg_stat_statements"
@@ -83,6 +83,9 @@ ActiveRecord::Schema.define(version: 2022_03_16_120758) do
     t.text "content"
     t.string "start_period"
     t.string "end_period"
+    t.string "name"
+    t.text "comment"
+    t.string "source_url"
     t.index ["rule_id"], name: "index_decision_tables_on_rule_id"
   end
 
