0ffd6bfebca5ec4dd5fda05e9a372b8b509fb93e
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-08-25 17:49:21 +0200

    implements create, update for users api

--- app/controllers/api/v2/users_controller.rb
+++ app/controllers/api/v2/users_controller.rb
@@ -7,15 +7,49 @@ module Api::V2
     end
 
     def create
+      attrs = user_attributes.merge(program_id: current_project_anchor.program.id)
+
+      user = nil
+      User.transaction do
+        user = current_project_anchor.program.users.create!(attrs)
+      end
+
+      options = {}
+      render json: serializer_class.new(user, options).serialized_json
     end
 
     def update
+      user = current_project_anchor.program.users.find(params[:id])
+      User.transaction do
+        user.update!(user_attributes)
+      end
+
+      options = {}
+      render json: serializer_class.new(user, options).serialized_json
     end
 
     private
 
     def serializer_class
       ::V2::UserSerializer
+    end
+
+    def user_attributes
+      att = user_params[:attributes]
+      {
+        email:          att[:email],
+        dhis2_user_ref: att[:dhis2UserRef],
+        password:       att[:password]
+      }
+    end
+
+    def user_params
+      params.require(:data)
+            .permit(attributes: [
+                      :email,
+                      :dhis2UserRef,
+                      :password
+                    ])
     end
   end
 end
