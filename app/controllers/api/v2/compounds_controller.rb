d78a8bcb824f599f0e7d55b06837caf4bbaebf88
Madeline OLeary <madeline.w.oleary@gmail.com>
2022-09-08 16:43:07 +0200

    update working while maintaining sets, formulas

--- app/controllers/api/v2/compounds_controller.rb
+++ app/controllers/api/v2/compounds_controller.rb
@@ -30,13 +30,23 @@ module Api
       def create
         payment_rule = nil
         payment_rules_attributes = compound_attributes
+        payment_rules_attributes = payment_rules_attributes.merge(rule_attributes: { name: compound_params[:attributes][:name], kind: "payment" })
         options = {}
 
         PaymentRule.transaction do
-          payment_rule = project.payment_rules.create(payment_rules_attributes)
-          payment_rule.save!
+          payment_rule = project.payment_rules.create!(payment_rules_attributes)
         end
 
+        options[:include] = default_relationships
+        render json: serializer_class.new(payment_rule, options).serialized_json
+      end
+
+      def update
+        payment_rule = project.payment_rules.find(params[:id])
+        payment_rules_attributes = compound_attributes
+        payment_rule.rule.update(name: compound_params[:attributes][:name])
+        options = {}
+        payment_rule.update(payment_rules_attributes)
         options[:include] = default_relationships
         render json: serializer_class.new(payment_rule, options).serialized_json
       end
@@ -44,7 +54,7 @@ module Api
       private
 
       def default_relationships
-        %i[formulas sets project_sets rule]
+        %i[formulas sets project_sets]
       end
 
       def detailed_relationships
@@ -56,9 +66,9 @@ module Api
       def compound_params
         params.require(:data)
               .permit(attributes: [
-                        :sets,
                         :frequency,
-                        :name
+                        :name,
+                        :setIds => []
                       ])
       end
 
@@ -66,11 +76,11 @@ module Api
         att = compound_params[:attributes]
         {
           frequency:       att[:frequency],
-          package_ids:     att[:sets] || [],
-          rule_attributes: {
-            name: att[:name],
-            kind: "payment"
-          }
+          package_ids:     att[:setIds] || []
+          # rule_attributes: {
+          #   name: att[:name],
+          #   kind: "payment"
+          # }
         }
       end
 
