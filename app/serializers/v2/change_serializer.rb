853f73a87b97a2dde9e2bf21126490cd4677636c
Stéphan Mestach <smestach@bluesquare.org>
2022-09-15 09:08:12 +0200

    Calculate paths

--- app/serializers/v2/change_serializer.rb
+++ app/serializers/v2/change_serializer.rb
@@ -25,9 +25,19 @@ class V2::ChangeSerializer < V2::BaseSerializer
     end
   end
 
-  attributes :path do |object, record_serialization_params|
+  attributes :name do |version, params|
+    object = params[:hesabu_objects]["#{version.item_type}-#{version.item_id}"]
+    [
+      version.item_type, 
+      object.try(:name) || object.try(:code) || object.try(:email), 
+      "#{version.item_id}"
+    ].select {|d| d}.join(" - ")
+  end 
+
+  attributes :path do |object, params|
+    path = nil
     version = object
-    object = instance_eval(version.item_type).find_by_id(version.item_id)
+    object = params[:hesabu_objects]["#{version.item_type}-#{version.item_id}"]
     if object && version.item_type == "Formula"
       formula = object
       kind = FORMULA_TYPES[formula.rule.kind]
@@ -35,9 +45,58 @@ class V2::ChangeSerializer < V2::BaseSerializer
         parentId: formula.rule.package_id ? formula.rule.package_id : formula.rule.payment_rule_id,
         parentKind:  formula.rule.package_id ? "sets": "compounds",
         kind: kind + "_formulas",
-        formulaId: object.id
+        itemId: object.id
       }
-      path
     end 
+
+    if object && version.item_type == "Rule"
+      rule = object
+      kind = FORMULA_TYPES[rule.kind]
+      path = {
+        parentId: rule.package_id ? rule.package_id : rule.payment_rule_id,
+        parentKind:  rule.package_id ? "sets": "compounds",
+        kind: kind + "_formulas",
+      }
+    end     
+
+    if object && version.item_type == "PackagePaymentRule"
+      path = {
+        parentId: object.payment_rule_id,
+        parentKind:  "compounds",
+      }
+    end
+
+    if object && version.item_type == "DecisionTable"
+      decision_table = object
+      path = {
+        parentId: decision_table.rule.package_id ? decision_table.rule.package_id : decision_table.rule.payment_rule_id,
+        parentKind:  decision_table.rule.package_id ? "sets": "compounds",
+        kind: "topic/decisions",
+        itemId: object.id
+      }
+    end
+
+    if object && ["ActivityPackage", "PackageState"].include?(version.item_type)
+      path = {
+        parentId: object.package_id,
+        parentKind: "sets",
+        kind: "topic_formulas"
+      }
+    end
+
+    if object && version.item_type == "PackageEntityGroup"
+      path = {
+        parentId: object.package_id,
+        parentKind: "sets"  
+      }
+    end
+
+    if object && version.item_type == "User"
+      user = object
+      path = {
+        parentKind: "users",
+      }
+    end
+    path
   end
 end
