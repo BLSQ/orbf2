cbcc4215c0ea41836e28bcb670663a0aa7e438fa
Stéphan Mestach <smestach@bluesquare.org>
2022-09-15 09:08:12 +0200

    Calculate paths

--- app/controllers/api/v2/changes_controller.rb
+++ app/controllers/api/v2/changes_controller.rb
@@ -4,10 +4,17 @@ module Api
   module V2
     class ChangesController < BaseController
       def index
-        # byebug
         changes = project.project_anchor.program.versions.order("id DESC").limit(100)
-        # byebug
-        options = {}
+
+        hesabu_objects = {}
+        # avoid select n+1
+        changes.group_by(&:item_type).each do |item_type, related_changes|
+          instance_eval(item_type).all.where(id:related_changes.map(&:item_id)).each do |hesabu_model_instance|
+            hesabu_objects["#{item_type}-#{hesabu_model_instance.id}"] = hesabu_model_instance
+          end
+        end
+        
+        options = { params: { hesabu_objects: hesabu_objects }}
 
         render json: serializer_class.new(changes, options).serialized_json
       end
