14d65526d03e4719ada174e4a7f74bb265496ee5
Stéphan Mestach <smestach@bluesquare.org>
2022-12-28 08:14:55 +0100

    show version

--- http_server.rb
+++ http_server.rb
@@ -7,10 +7,10 @@ while session = server.accept
   request = session.gets
   puts request
 
-  session.print "HTTP/1.1 200\r\n" # 1
-  session.print "Content-Type: text/html\r\n" # 2
-  session.print "\r\n" # 3
-  session.print "Hello world! The time is #{Time.now}" # 4
+  session.print "HTTP/1.1 200\r\n"
+  session.print "Content-Type: text/html\r\n"
+  session.print "\r\n"
+  session.print "hesabu: #{ENV['RELEASE_TAG']}"
 
   session.close
 end
