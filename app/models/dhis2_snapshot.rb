9a73817276cb6ea42546bbd93e841b3e73338f57
Stéphan Mestach <smestach@bluesquare.org>
2026-06-18 11:34:09 +0200

    annotate on fresh db

--- app/models/dhis2_snapshot.rb
+++ app/models/dhis2_snapshot.rb
@@ -3,7 +3,7 @@
 #
 # Table name: dhis2_snapshots
 #
-#  id                :integer          not null, primary key
+#  id                :bigint(8)        not null, primary key
 #  content           :jsonb            not null
 #  dhis2_version     :string           not null
 #  kind              :string           not null
@@ -17,6 +17,10 @@
 # Indexes
 #
 #  index_dhis2_snapshots_on_project_anchor_id  (project_anchor_id)
+#
+# Foreign Keys
+#
+#  fk_rails_...  (project_anchor_id => project_anchors.id)
 #
 
 class Dhis2Snapshot < ApplicationRecord
